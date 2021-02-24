1f81b903a7860e1685397d8e7536142c7594a5d7
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-02-24 15:22:07 +0100

    feat: Added HomeScreen.js, and Product.js  components with data  fetched from products.js

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Row } from 'react-bootstrap'
+import { Col } from 'react-bootstrap'
+import products from '../products'
+import Product from '../components/Product'
+
+const HomeScreen = () => {
+    return (
+        <>
+          <h1>Latest Products</h1>
+            <Row>
+                {products.map((product)=>(
+                    <Col
+                        sm={20}
+                        md={12}
+                        lg={6}
+                        xlg={3}
+                        >
+                        <h3>{product.name}</h3>
+                            <Product product={product}/>
+                    </Col>
+                ))}
+            </Row>
+        </>
+    )
+}
+
+export default HomeScreen
