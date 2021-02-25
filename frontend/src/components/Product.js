6bdb265248f065c2cd640add8e2aeb2903edd1aa
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-02-25 12:56:15 +0100

    feat:  Rating.js component added to  render

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Card} from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({product}) => {
     return (
@@ -13,12 +14,14 @@ const Product = ({product}) => {
                         <strong>{product.name}</strong>
                     </Card.Title>
                 </a>
-                <Card.Text as="div">
-                    <div className="my-3">
-                    {product.rating} from {product.numReviews} reviews
-                    </div>
-                </Card.Text>
+                <Rating 
+                    value={product.rating} 
+                    text={`${product.numReviews}reviews`}
+                
+                    >
+                </Rating>
                 <Card.Text as="h3">
+                    
                     ${product.price}
                 </Card.Text>
            </Card.Body>
