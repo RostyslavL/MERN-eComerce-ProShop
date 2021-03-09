f7a60c371bb6a2cbd75b3213f56fecd6b1fe2d3c
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-03-09 16:24:01 +0100

    feat:  create productRouts with adding asyncHandler to fetch products data from database

---
+++ backend/routes/productRouts.js
@@ -0,0 +1,35 @@
+
+import express from 'express'
+const router = express.Router()
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
+
+// @desc    : Fetch all products 
+// @route   : Get request to /api/products
+// @access  : Public route
+
+
+router.get(
+    '/', 
+    asyncHandler( async (req, res) =>{
+    const products = await Product.find({})
+    res.json(products)
+}))
+
+// @desc    : Fetch single product
+// @route   : Get request to /api/product/:id
+// @access  : Public route
+
+router.get(
+    '/:id',  
+    asyncHandler( async (req, res) =>{
+    const product = await Product.findById(req.params.id)
+
+    if(product){
+        res.json(product)
+    }else{
+        res.status(404).json({message: 'Error : Product not found'})
+    }
+}))
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
-import producrs from './data/products.js'
 import connectDB from './config/db.js'
+import productRouts from './routes/productRouts.js'
 
 dotenv.config()
 
@@ -13,13 +13,8 @@ const app = express()
 app.get('/', (req, res)=>{
     res.send('API is running.........')
 })
-app.get('/api/products', (req, res)=>{
-    res.json(producrs)
-})
-app.get('/api/products/:id', (req, res)=>{
-    const product = producrs.find(p => p._id === req.params.id)
-    res.json(product)
-})
+
+app.use('/api/products', productRouts)
 
 const  PORT = process.env.PORT || 5000
 
