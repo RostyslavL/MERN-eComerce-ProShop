359135a63900742e54258e3bb31466a8d02048d7
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-03-12 09:58:38 +0100

    feat: created Message.js component

---
+++ frontend/src/components/Message.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Alert} from 'react-bootstrap'
+
+const Message = ({variant, children}) => {
+    
+    return (
+        <Alert variant={variant}> 
+          <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+          <hr />
+            <p className="mb-0">
+                {children} 
+            </p>
+        </Alert>
+    )
+}
+
+Message.defaultProps = {
+    variant: 'info'
+}
+    
+export default Message
+
+
