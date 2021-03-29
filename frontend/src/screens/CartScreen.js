2172ab522799e4e63c3fada6874941269f961556
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-03-29 11:12:27 +0200

    feat:  'Add to Cart'  items functionality implemented

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,39 @@
-import React from 'react'
+import React , {useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector} from 'react-redux'
+import Message from '../components/Message'
+import { 
+    Row, 
+    Col, 
+    Listgroup, 
+    Image, 
+    Form, 
+    Button, 
+    Card 
+} from 'react-bootstrap'
+import { addToCart } from '../actions/cartActions'
 
-const CartScreen = () => {
+const CartScreen = ({ match, location, histoty }) => {
+
+    const productID = match.params.id
+
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+    const dispatch = useDispatch()
+
+    const cart = useSelector(state => state.cart)
+
+    const { cartItems } = cart
+    
+    console.log(cartItems)
+
+    useEffect(() => {
+        if(productID){
+            dispatch(addToCart(productID, qty))
+        }
+    }, [dispatch, productID, qty])
+
+    
     return (
         <div>
             Cart
