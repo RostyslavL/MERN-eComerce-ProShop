4444aa6253feb86815abbd159680f0b94a82a951
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-03-30 11:36:08 +0200

    feat: created productController

---
+++ backend/controllers/productController.js
@@ -0,0 +1,33 @@
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
+
+// @desc    : Fetch all products 
+// @route   : Get request to /api/products
+// @access  : Public route
+
+const getProducts = asyncHandler(async(req, res) =>{
+    const products = await Product.find({})
+    // throw new Error('New Error')
+    res.json(products)
+})
+
+// @desc    : Fetch single product
+// @route   : Get request to /api/product/:id
+// @access  : Public route
+
+
+const getProductById = asyncHandler(async(req, res) =>{
+    const product = await Product.findById(req.params.id)
+
+    if(product){
+        res.json(product)
+    }else{
+        res.status(404)
+        throw new Error('Product not found')
+    }
+})
+
+export {
+    getProducts,
+    getProductById
+}
