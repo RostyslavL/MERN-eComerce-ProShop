feaf4c7bbc7118751e29b0b48c8cbf37aabad7ea
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-03-30 12:35:23 +0200

    feat: created user authentication endpoints

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
+
 import productRouts from './routes/productRouts.js'
+import userRouts from './routes/userRouts.js'
+
 import {notFound, errorHandler} from './middleware/middleWare.js';
 
 dotenv.config()
@@ -10,12 +13,14 @@ dotenv.config()
 connectDB()
 
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res)=>{
     res.send('API is running.........')
 })
 
 app.use('/api/products', productRouts)
+app.use('/api/users', userRouts)
 
 app.use(notFound)
 
