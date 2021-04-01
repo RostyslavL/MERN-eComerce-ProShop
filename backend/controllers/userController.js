f8673f189081cc29709ce9e52eff51e91846370e
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-01 18:40:24 +0200

    feat: creatiaon of a new user functionality added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,6 +25,40 @@ const authUser = asyncHandler(async(req, res) =>{
     }
 })
 
+// @desc    : Register new user
+// @route   : POST request to /api/users/
+// @access  : Public route
+
+const registerUser = asyncHandler(async(req, res) =>{
+    const { name, email, password} = req.body
+
+    const userExists = await User.findOne({email: email})
+
+    if(userExists){
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+        name, 
+        email, 
+        password
+    })
+
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),  
+        })
+    }else{
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+})
+
 // @desc    : Get user profile
 // @route   : GET request to /api/users/profile
 // @access  : Private route
@@ -45,4 +79,8 @@ const getUserProfile = asyncHandler(async(req, res) =>{
     }
 })
 
-export { authUser, getUserProfile }
+export { 
+    authUser, 
+    getUserProfile,
+    registerUser 
+}
