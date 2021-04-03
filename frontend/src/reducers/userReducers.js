9fb40e02c4411a82445ecba92549020b1a704791
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-03 13:12:00 +0200

    feat: created userRegisterReducer for registration functionality

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -2,11 +2,14 @@ import {
     USER_LOGIN_FAIL, 
     USER_LOGIN_REQUEST, 
     USER_LOGIN_SUCCESS, 
-    USER_LOGOUT 
+    USER_LOGOUT, 
+    USER_REGISTER_FAIL, 
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS
 } from "../constants/userConstants"
 
 export const userLoginReducer = (
-    state = {  },
+    state = {},
     action
   ) => {
     switch (action.type) {
@@ -18,6 +21,22 @@ export const userLoginReducer = (
         return { loading: false, error: action.payload }
       case USER_LOGOUT:
         return {}
+      default:
+        return state
+    }
+  }
+
+export const userRegisterReducer = (
+    state = {},
+    action
+  ) => {
+    switch (action.type) {
+      case USER_REGISTER_REQUEST:
+        return { ...state, loading: true }
+      case USER_REGISTER_SUCCESS:
+        return { loading: false, userInfo: action.payload }
+      case USER_REGISTER_FAIL:
+        return { loading: false, error: action.payload }
       default:
         return state
     }
