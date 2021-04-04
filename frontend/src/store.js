d71233bbe56634d180ccb892a467a3cf3ba456de
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-04 12:57:44 +0200

    feat: add a userDetailsReducer into main store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,18 +3,19 @@ import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import {
     productListReducer, 
-    productDetailsReducer
+    productDetailsReducer,
 } from './reducers/productReducers'
 
 import {cartReducer} from './reducers/cartReducer'
-import { userLoginReducer, userRegisterReducer } from './reducers/userReducers'
+import { userLoginReducer, userRegisterReducer, userDetailsReducer } from './reducers/userReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
-    userRegister: userRegisterReducer
+    userRegister: userRegisterReducer,
+    userDetails: userDetailsReducer,
 })
 
 const cartItemsFromStorage =  localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
