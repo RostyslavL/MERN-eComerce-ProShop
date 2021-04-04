bf8ff0f6c0004e4010a359056e8161048ac61693
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-04 12:39:58 +0200

    feat: added updateUserProfile endpoint & protected route in backend

--- backend/routes/userRouts.js
+++ backend/routes/userRouts.js
@@ -4,7 +4,8 @@ const router = express.Router()
 import { 
     authUser, 
     getUserProfile, 
-    registerUser 
+    registerUser,
+    updateUserProfile 
 } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
@@ -12,6 +13,8 @@ router.route('/').post(registerUser)
 
 router.post('/login', authUser)
 
-router.route('/profile').get(protect, getUserProfile)
+router.route('/profile')
+    .get(protect, getUserProfile)
+    .put(protect , updateUserProfile)
 
 export default router
