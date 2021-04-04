6c180ae1afaded1d73233b1c9b3c2bf52a09e357
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-04 14:41:06 +0200

    feat: add 'userUpdateProfileReducer' into main store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,7 +7,12 @@ import {
 } from './reducers/productReducers'
 
 import {cartReducer} from './reducers/cartReducer'
-import { userLoginReducer, userRegisterReducer, userDetailsReducer } from './reducers/userReducers'
+import { 
+    userLoginReducer, 
+    userRegisterReducer, 
+    userDetailsReducer, 
+    userUpdateProfileReducer 
+} from './reducers/userReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -16,6 +21,7 @@ const reducer = combineReducers({
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer,
 })
 
 const cartItemsFromStorage =  localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
