796c020c88d5eb1b9080ca9a670dde5038c60620
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-07 13:36:36 +0200

    fix: some styles correction

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,7 +2,11 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 
 const Footer = () => {
-    return (<footer>
+    return (
+    <footer style={{
+        backgroundColor:'#f2ebeb',
+        width: '100%'
+    }}>
         <Container>
             <Row>
                 <Col 
