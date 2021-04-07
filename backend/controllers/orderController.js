4b502ba79b289f3ca0b1a10d02a4bdbe724687be
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-07 17:54:04 +0200

    feat: create orderController into backend

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,38 @@
+import asyncHandler from 'express-async-handler'
+import orderModel from '../models/orderModel.js'
+
+// @desc    : Create a new order
+// @route   : POST request to /api/orders
+// @access  : Private route
+
+const addOrderItems = asyncHandler(async(req, res) =>{
+    const {
+        orderItems, 
+        shippingAddress, 
+        paymentMethod, 
+        itemsPrice, 
+        taxPrice, 
+        shippingPrice, 
+        totalPrice
+    } = req.body
+
+    if(orderItems && orderItems.lengtgh === 0){
+        res.status(400) // bad request
+        throw new Error('No order items')
+        return
+    }else{
+        const order = new Order({
+            user: req.user._id,
+            orderItems, 
+            shippingAddress, 
+            paymentMethod, 
+            itemsPrice, 
+            taxPrice, 
+            shippingPrice, 
+            totalPrice
+        })
+
+        const createdOrder = await order.save()
+        res.status(201).json(createdOrder) // HTTP 201 Created success status response code 
+    }
+})
