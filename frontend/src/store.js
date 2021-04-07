677bce9dbc8df4d013d38ddfa12fce17c9ef4950
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-07 10:22:43 +0200

    feat: implemented 'shippingAddressFromStorage' checking main state & added to cart initialState

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -28,8 +28,10 @@ const cartItemsFromStorage =  localStorage.getItem('cartItems') ? JSON.parse(loc
 
 const userInfoFromStorage =  localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
 
+const shippingAddressFromStorage =  localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')) : {}
+
 const initialState = {
-    cart: { cartItems: cartItemsFromStorage },
+    cart: { cartItems: cartItemsFromStorage, shippingAddress: shippingAddressFromStorage},
     userLogin: { userInfo: userInfoFromStorage },
 }
 
