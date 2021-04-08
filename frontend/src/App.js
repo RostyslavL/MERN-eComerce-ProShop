98d27a7a22191da758caa00b5782d3ba70a5516a
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-08 14:02:07 +0200

    feat: add  a route for orderScreen into main app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
+import OrderScreen from './screens/OrderScreen'
 
 
 const App = () =>{
@@ -23,6 +24,7 @@ const App = () =>{
             <Route path='/login' component={LoginScreen}></Route>
             <Route path='/shipping' component={ShippingScreen}></Route>
             <Route path='/placeorder' component={PlaceOrderScreen}></Route>
+            <Route path='/order/:id' component={OrderScreen}></Route>
             <Route path='/payment' component={PaymentScreen}></Route>
             <Route path='/register' component={RegisterScreen}></Route>
             <Route path='/profile' component={ProfileScreen}></Route>
