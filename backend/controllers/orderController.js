14b0a5635d576d4aa09f15d35b25fc2d72f4f016
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-10 09:27:25 +0200

    feat: add 'updateOrderToPaid'  function to orderController

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -39,7 +39,7 @@ const addOrderItems = asyncHandler(async(req, res) =>{
 })
 
 // @desc    : Create order by ID
-// @route   : GET request to /api/orders
+// @route   : GET request to /api/orders/:id
 // @access  : Private route
 
 const getOrderByID = asyncHandler(async(req, res) =>{
@@ -56,7 +56,32 @@ const getOrderByID = asyncHandler(async(req, res) =>{
     }
 })
 
+// @desc    : Update order to paid
+// @route   : GET request to /api/orders/:id/pay
+// @access  : Private route
+
+const updateOrderToPaid = asyncHandler(async(req, res) =>{
+    const order = await Order.findById(req.params.id)
+
+    if(order){
+        order.isPaid = true
+        order.paidAt = Date.now()
+        order.paymentResult = {
+            id: req.body.id,
+            status:req.body.status,
+            update_time:req.body.update_time,
+            email_address:req.body.payer.email_address
+        }
+        const updatedOrder = await order.save()
+        res.json( updatedOrder)
+    }else{
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})
+
 export { 
     addOrderItems,
-    getOrderByID 
+    getOrderByID,
+    updateOrderToPaid 
 }
