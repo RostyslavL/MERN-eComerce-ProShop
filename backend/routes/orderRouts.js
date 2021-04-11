9f1c42b56d4ee5476924d336ef4177162cd5521d
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-11 10:58:16 +0200

    feat: created  a route for 'myorders' in orderRouts

--- backend/routes/orderRouts.js
+++ backend/routes/orderRouts.js
@@ -1,10 +1,16 @@
 import express from 'express'
 const router = express.Router()
-import { addOrderItems,getOrderByID,updateOrderToPaid } from '../controllers/orderController.js'
+import { 
+    addOrderItems,
+    getOrderByID,
+    updateOrderToPaid,
+    getMyOrders 
+} from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(protect, addOrderItems)
 router.route('/:id').get(protect, getOrderByID)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
+router.route('/myorders').get(protect, getMyOrders)
 
 export default router
