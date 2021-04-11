60d6518889476c8e52cd6e23b09a5b7899979c95
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-12 01:18:03 +0200

    feat:  add 'getUsers'  into userRoutes with  admin protection

--- backend/routes/userRouts.js
+++ backend/routes/userRouts.js
@@ -4,11 +4,12 @@ import {
     authUser, 
     getUserProfile, 
     registerUser,
-    updateUserProfile 
+    updateUserProfile ,
+    getUsers
 } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
-router.route('/').post(registerUser)
+router.route('/').post(registerUser).get(protect, getUsers)
 
 router.post('/login', authUser)
 
