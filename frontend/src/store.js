b02c2d798587b47982261f0c5c2807125ad15ca6
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-12 16:42:51 +0200

    feat: add 'userUpdateReducer' into main app store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -13,7 +13,8 @@ import {
     userDetailsReducer, 
     userUpdateProfileReducer,
     userListReducer,
-    userDeleteReducer
+    userDeleteReducer,
+    userUpdateReducer
 } from './reducers/userReducers'
 
 import { orderCreateReducers, orderDetailsReducers, orderListMyReducer, orderPayReducers } from './reducers/orderReducers'
@@ -28,6 +29,7 @@ const reducer = combineReducers({
     userUpdateProfile: userUpdateProfileReducer,
     userList: userListReducer,
     userDelete: userDeleteReducer,
+    userUpdate: userUpdateReducer,
     orderCreate: orderCreateReducers,
     orderDetails: orderDetailsReducers,
     orderPay: orderPayReducers,
