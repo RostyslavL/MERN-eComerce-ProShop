f57738c5a0d4f3699b67a9c76ba337955a982449
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-12 09:47:01 +0200

    feat: created 'updateUser' & 'getUserById' functions in userControllers to edit users functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -131,6 +131,46 @@ const deleteUser = asyncHandler(async (req, res) => {
     }
 })
 
+// // @desc    : Get user by ID
+// // @route   : GET request to /api/users/:id
+// // @access  : Private/Admin
+
+const getUserById = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id).select('-password')
+    if(user){
+        res.json(user)
+    }else{
+        res.status(404)
+        throw new Error('User not found')
+    }    
+})
+
+
+// @desc    : Update user 
+// @route   : PUT request to /api/users/:id
+// @access  : Private/Admin
+
+const updateUser = asyncHandler(async(req, res) =>{
+    const user = await User.findById(req.params.id)
+
+    if(user){
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        user.isAdmin = req.body.isAdmin
+    
+        const updatedUser = await user.save()
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+        })
+    }else{
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
 
 export { 
     authUser, 
@@ -138,5 +178,7 @@ export {
     registerUser,
     updateUserProfile,
     getUsers,
-    deleteUser 
+    deleteUser,
+    getUserById,
+    updateUser 
 }
