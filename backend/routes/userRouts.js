948af5175bff6ca7306f0f2e5e5c17353e47772d
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-12 09:49:45 +0200

    feat: add 'updateUser' & 'getUserById' functions into a additional  route in userRouts

--- backend/routes/userRouts.js
+++ backend/routes/userRouts.js
@@ -6,7 +6,9 @@ import {
     registerUser,
     updateUserProfile ,
     getUsers,
-    deleteUser
+    deleteUser,
+    getUserById,
+    updateUser
 } from '../controllers/userController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
@@ -18,6 +20,9 @@ router.route('/profile')
     .get(protect, getUserProfile)
     .put(protect , updateUserProfile)
 
-router.route('/:id').delete(protect, admin, deleteUser)
-
+router.route('/:id')
+    .delete(protect, admin, deleteUser)
+    .get(protect, admin, getUserById)
+    .put(protect, admin, updateUser)
+    
 export default router
