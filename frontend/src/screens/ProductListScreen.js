bd5dcdca7444d5962c184b4baac69a6bd638a269
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-14 01:04:46 +0200

    feat: implemented 'productDeleteReducer' into ProductScreen & delete functionality with products list refresh

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -4,7 +4,7 @@ import { Table, Button, Row, Col} from 'react-bootstrap'
 import {useDispatch, useSelector} from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import {listProducts} from '../actions/productAction'
+import {listProducts, deleteProduct} from '../actions/productAction'
 
 const ProductListScreen = ({history, match}) => {
 
@@ -16,17 +16,20 @@ const ProductListScreen = ({history, match}) => {
     const userLogin = useSelector((state) => state.userLogin)
     const { userInfo } = userLogin
 
+    const productDelete = useSelector((state) => state.productDelete)
+    const {loading: loadingDelete, success: successDelete, error: errorDelete } = productDelete
+
     useEffect(() =>{
         if(userInfo && userInfo.isAdmin){
             dispatch(listProducts())            
         }else{
             history.push('/login')
         }   
-    }, [dispatch, history,userInfo])
+    }, [dispatch, history,userInfo, successDelete])
 
     const deleteHandler = (id) =>{
         if(window.confirm('Are you sure ?')){
-            // delete products 
+            dispatch(deleteProduct(id))  
         }
     }
     const createProductHandler = (product) =>{
@@ -48,7 +51,9 @@ const ProductListScreen = ({history, match}) => {
                         </i> 
                 </Button>
             </Col>
-        </Row>            
+        </Row> 
+        {loadingDelete && <Loader/> }
+        {errorDelete && <Message variant='danger'>{errorDelete}</Message>}          
             {
                 loading ? 
                 <Loader/> : 
