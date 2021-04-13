e92fd54122cf1f78cd30601ee3e6a715f7c84918
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-14 01:33:47 +0200

    feat: created 'createProduct'  and 'updateProduct' function in productController

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -44,8 +44,71 @@ const deleteProduct = asyncHandler(async(req, res) =>{
     }
 })
 
+// @desc    : Create single product
+// @route   : POST request to /api/products
+// @access  : Private/Admin
+
+
+const createProduct = asyncHandler(async(req, res) =>{
+    const product = new Product({
+        name: 'Sample Name',
+        price: 0,
+        user:req.user._id,
+        image: '/img/sample.jpg',
+        brand: 'Sample Brand',
+        category: 'Sample Category',
+        countInStock: 0,
+        rating: 0.0,
+        numReviews: 0,
+        description: 'Sample Text'
+    })
+    const createdProduct = await product.save()
+    res.status(201).json(createdProduct)
+})
+
+// @desc    : Update single product
+// @route   : PUT request to /api/products/:id
+// @access  : Private/Admin
+
+
+const updateProduct = asyncHandler(async(req, res) =>{
+    const {
+        name,
+        price,
+        image,
+        brand,
+        category,
+        countInStock,
+        rating,
+        numReviews,
+        description
+    } = req.body
+
+    const product = await Product.findById(res.params.id)
+
+    if(product){
+        product.name = name
+        product.price = price
+        product.image = image
+        product.brand = brand
+        product.category = category
+        product.countInStock = countInStock
+        product.rating = rating
+        product.numReviews = numReviews
+        product.description =  description
+
+        const updatedProduct = await product.save()
+        res.json(updatedProduct)
+    }else{
+        res.status(404)
+        throw new Error('Product not found')
+    }
+})
+
 export {
     getProducts,
     getProductById,
-    deleteProduct
+    deleteProduct,
+    createProduct,
+    updateProduct
 }
