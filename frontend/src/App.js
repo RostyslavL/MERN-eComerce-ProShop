e5f1131e383d6f8fde399d647459fb97400e3422
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-14 00:29:35 +0200

    feat: add ProductLIst into main app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
+import ProductListScreen from './screens/ProductListScreen'
 
 
 const App = () =>{
@@ -33,6 +34,7 @@ const App = () =>{
             <Route path='/product/:id' component={ProductScreen}></Route>
             <Route path='/cart/:id?' component={CartScreen}></Route>
             <Route path='/admin/userlist' component={UserListScreen}></Route>
+            <Route path='/admin/productlist' component={ProductListScreen}></Route>
             <Route path='/admin/user/:id/edit' component={UserEditScreen}></Route>
             <Route path='/' component={HomeScreen} exact></Route>
           </Container>   
