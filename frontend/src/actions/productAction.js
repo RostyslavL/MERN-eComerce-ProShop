eb28d43f0f8218f3394f47874c7e2ddf0e883488
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-14 00:53:15 +0200

    feat: created 'deleteProduct'  in productReducers with admin token protection

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -5,7 +5,11 @@ import {
 
     PRODUCT_DETAILS_REQUEST,
     PRODUCT_DETAILS_SUCCESS,
-    PRODUCT_DETAILS_FAIL
+    PRODUCT_DETAILS_FAIL,
+    
+    PRODUCT_DELETE_REQUEST,
+    PRODUCT_DELETE_SUCCESS,
+    PRODUCT_DELETE_FAIL
 } 
 from '../constants/productConstants.js'
 import axios from 'axios'
@@ -50,3 +54,34 @@ export const listProductDetails = (id) => async (dispatch) => {
       })
     }
   }
+
+export const deleteProduct = (id) => async (
+    dispatch, 
+    getState
+    ) =>{
+    try {
+        dispatch({
+            type: PRODUCT_DELETE_REQUEST
+        })
+        const { userLogin: { userInfo}} = getState() // Is necessary to pass your token
+
+        const config = {
+            headers: {
+              Authorization: `Bearer ${userInfo.token}`
+            },
+        }
+
+       await axios.delete(`/api/products/${id}`, config)
+
+      dispatch({ type: PRODUCT_DELETE_SUCCESS })
+        
+    } catch (error) {
+        dispatch({
+          type: PRODUCT_DELETE_FAIL,
+          payload:
+            error.response && error.response.data.message
+              ? error.response.data.message
+              : error.message,
+        })
+    }
+}
