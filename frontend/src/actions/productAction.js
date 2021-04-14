20220a676d2a70a884df8176b9ba2c0ec92b9bc6
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-14 14:18:24 +0200

    feat: create 'updateProduct' action in productActions

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -14,6 +14,11 @@ import {
   PRODUCT_CREATE_REQUEST,
   PRODUCT_CREATE_SUCCESS,
   PRODUCT_CREATE_FAIL,
+
+  PRODUCT_UPDATE_REQUEST,
+  PRODUCT_UPDATE_SUCCESS,
+  PRODUCT_UPDATE_FAIL,  
+  PRODUCT_UPDATE_RESET,
 } 
 from '../constants/productConstants.js'
 import axios from 'axios'
@@ -116,6 +121,41 @@ export const createProduct = () => async (
     } catch (error) {
         dispatch({
           type: PRODUCT_CREATE_FAIL,
+          payload:
+            error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+        })
+    }
+}
+
+export const updateProduct = (product) => async (
+    dispatch, 
+    getState
+    ) =>{
+    try {
+        dispatch({
+            type: PRODUCT_UPDATE_REQUEST
+        })
+        const { userLogin: { userInfo}} = getState() // Is necessary to pass your token
+
+        const config = {
+            headers: {
+              'Content-type':'application/json',
+              Authorization: `Bearer ${userInfo.token}`
+            },
+        }
+
+       const { data } = await axios.put(`/api/products/${product._id}`, {product}, config) 
+       
+      dispatch({ 
+        type: PRODUCT_UPDATE_SUCCESS,
+        payload: data
+     })
+        
+    } catch (error) {
+        dispatch({
+          type: PRODUCT_UPDATE_FAIL,
           payload:
             error.response && error.response.data.message
             ? error.response.data.message
