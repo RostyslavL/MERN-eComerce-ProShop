4dbd9ecc7b20611a7b15af9bfe1da2473466f677
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-14 09:58:33 +0200

    feat: add 'createProduct' action

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -1,15 +1,20 @@
 import {
-    PRODUCT_LIST_REQUEST, 
-    PRODUCT_LIST_SUCCESS, 
-    PRODUCT_LIST_FAIL,
+  PRODUCT_LIST_REQUEST, 
+  PRODUCT_LIST_SUCCESS, 
+  PRODUCT_LIST_FAIL,
 
-    PRODUCT_DETAILS_REQUEST,
-    PRODUCT_DETAILS_SUCCESS,
-    PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
     
-    PRODUCT_DELETE_REQUEST,
-    PRODUCT_DELETE_SUCCESS,
-    PRODUCT_DELETE_FAIL
+  PRODUCT_DELETE_REQUEST,
+  PRODUCT_DELETE_SUCCESS,
+  PRODUCT_DELETE_FAIL,
+
+  PRODUCT_CREATE_REQUEST,
+  PRODUCT_CREATE_SUCCESS,
+  PRODUCT_CREATE_FAIL,  
+  PRODUCT_CREATE_RESET, 
 } 
 from '../constants/productConstants.js'
 import axios from 'axios'
@@ -82,6 +87,40 @@ export const deleteProduct = (id) => async (
             error.response && error.response.data.message
               ? error.response.data.message
               : error.message,
+        })
+    }
+}
+
+export const createProduct = () => async (
+    dispatch, 
+    getState
+    ) =>{
+    try {
+        dispatch({
+            type: PRODUCT_CREATE_REQUEST
+        })
+        const { userLogin: { userInfo}} = getState() // Is necessary to pass your token
+
+        const config = {
+            headers: {
+              Authorization: `Bearer ${userInfo.token}`
+            },
+        }
+
+       const { data } = await axios.post(`/api/products}`, {}, config)
+
+      dispatch({ 
+        type: PRODUCT_CREATE_SUCCESS,
+        payload: data
+     })
+        
+    } catch (error) {
+        dispatch({
+          type: PRODUCT_CREATE_FAIL,
+          payload:
+            error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
         })
     }
 }
