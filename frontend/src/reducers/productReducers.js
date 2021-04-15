8bcf0abeb4275f94c43326e80b6a9f477d466684
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-15 15:39:36 +0200

    fix: commemts delete

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -84,26 +84,6 @@ export const productCreateReducer = (
         return state
     }
   }
-
-// export const productUpdateReducer = (
-//     state = { product:{}},
-//     action
-//   ) => {
-//     switch (action.type) {
-//       case PRODUCT_UPDATE_REQUEST:
-//         return { loading: true }
-//       case PRODUCT_UPDATE_SUCCESS:
-//         return { loading: false, success: true, product: action.payload }
-//       case PRODUCT_UPDATE_FAIL:
-//         return { loading: false, error: action.payload }
-//       case PRODUCT_UPDATE_RESET:
-//         return { product:{} }
-//       default:
-//         return state
-//     }
-//   }
-
-  
   
 export const productUpdateReducer = (state = { product: {} }, action) => {
   switch (action.type) {
