5a818e2e775ea01c30dde95324846c6499b702af
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-15 15:27:07 +0200

    feat: create a route in orderRouts with admin user protection

--- backend/routes/orderRouts.js
+++ backend/routes/orderRouts.js
@@ -4,11 +4,14 @@ import {
     addOrderItems,
     getOrderByID,
     updateOrderToPaid,
-    getMyOrders 
+    getMyOrders,
+    getAllOrders 
 } from '../controllers/orderController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
-router.route('/').post(protect, addOrderItems)
+router.route('/')
+    .post(protect, addOrderItems)
+    .get(protect, admin, getAllOrders)
 router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderByID)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
