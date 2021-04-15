208f7f244301066b872318aec9153fc703800e66
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-15 16:38:27 +0200

    feat: add 'OrderListScreen' into main app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
 import ProductListScreen from './screens/ProductListScreen'
 import ProductEditScreen from './screens/ProductEditScreen'
+import OrderListScreen from './screens/OrderListScreen'
 
 
 const App = () =>{
@@ -38,6 +39,7 @@ const App = () =>{
             <Route path='/admin/productlist' component={ProductListScreen}></Route>
             <Route path='/admin/user/:id/edit' component={UserEditScreen}></Route>
             <Route path='/admin/product/:id/edit' component={ProductEditScreen}></Route>
+            <Route path='/admin/orderlist' component={OrderListScreen}></Route>
             <Route path='/' component={HomeScreen} exact></Route>
           </Container>   
         </main>
