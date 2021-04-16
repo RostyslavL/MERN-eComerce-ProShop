a9ef83814f7e2ac50fadbf6240dafbc08fa85869
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-16 18:02:38 +0200

    fix: pageSize  & count correction

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,7 +7,7 @@ import Product from '../models/productModel.js'
 
 const getProducts = asyncHandler(async(req, res) =>{
 
-  const pageSize = 2
+  const pageSize = 8
   const page = Number(req.query.pageNumber) ||  1
 
     const keyword = req.query.keyword ? 
@@ -19,7 +19,7 @@ const getProducts = asyncHandler(async(req, res) =>{
     } : {}
 
 
-    const count = await Product.count({...keyword})
+    const count = await Product.countDocuments({...keyword})
     const products = await Product.find({...keyword }).limit(pageSize).skip(pageSize * page -1 )
     // throw new Error('New Error')
 
