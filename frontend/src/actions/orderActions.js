1d46cb4a1db96916351610415661462a4064519e
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-16 11:04:29 +0200

    feat: create 'deliverOrder'  action in orderActions

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -19,6 +19,10 @@ import {
     ORDER_LIST_REQUEST,
     ORDER_LIST_SUCCESS,
     ORDER_LIST_FAIL,  
+
+    ORDER_DELIVER_REQUEST,
+    ORDER_DELIVER_SUCCESS,
+    ORDER_DELIVER_FAIL,     
 } from '../constants/orderConstants'
 
 
@@ -177,6 +181,38 @@ export const listOrders = () => async (
     } catch (error) {
         dispatch({
           type: ORDER_LIST_FAIL,
+          payload:
+            error.response && error.response.data.message
+              ? error.response.data.message
+              : error.message,
+        })
+    }
+}
+
+
+export const deliverOrder = (order) => async (dispatch, getState) =>{
+    try {
+        dispatch({
+            type: ORDER_DELIVER_REQUEST
+        })
+        const { userLogin: { userInfo}} = getState() // Is necessary to pass your token
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`
+            },
+        }
+
+        const { data } = await axios.put(`/api/orders/${order._id}/deliver`, config)
+
+        dispatch({
+            type: ORDER_DELIVER_SUCCESS,
+            payload: data // data  in success stage is performed as an order
+        })
+        
+    } catch (error) {
+        dispatch({
+          type: ORDER_DELIVER_FAIL,
           payload:
             error.response && error.response.data.message
               ? error.response.data.message
