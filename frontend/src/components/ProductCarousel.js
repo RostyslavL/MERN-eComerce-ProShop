09b0b3d4117bb66850e6e630adf5c9a196d592ed
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-17 00:04:12 +0200

    fix: useSelector, useDispatch path correction

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -1,4 +1,5 @@
-import React, {useEffect, useSelector, useDispatch} from 'react'
+import React, {useEffect} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {Carousel, Image} from 'react-bootstrap'
 import Loader from './Loader'
