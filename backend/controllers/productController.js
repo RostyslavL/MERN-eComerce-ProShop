5d72aa7fac23dd21bb3cea587cb325af1dda3ce0
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-16 17:55:54 +0200

    feat : add product Pagination back-end endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,6 +7,9 @@ import Product from '../models/productModel.js'
 
 const getProducts = asyncHandler(async(req, res) =>{
 
+  const pageSize = 2
+  const page = Number(req.query.pageNumber) ||  1
+
     const keyword = req.query.keyword ? 
     {
       name: {
@@ -15,9 +18,13 @@ const getProducts = asyncHandler(async(req, res) =>{
       }
     } : {}
 
-    const products = await Product.find({...keyword })
+
+    const count = await Product.count({...keyword})
+    const products = await Product.find({...keyword }).limit(pageSize).skip(pageSize * page -1 )
     // throw new Error('New Error')
-    res.json(products)
+
+    
+    res.json({products, page, pages: Math.ceil(count / pageSize)})
 })
 
 // @desc    : Fetch single product
