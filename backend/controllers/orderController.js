888c0872d1cba79a9cab81e6eda1bc8d5db8d6bc
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-16 10:49:15 +0200

    feat: create 'updateOrderToDelivered' functionality in orderController

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -98,10 +98,31 @@ const getAllOrders = asyncHandler(async(req, res) =>{
     res.json(orders)
 })
 
+
+
+// @desc    : Update order to delivered
+// @route   : GET request to /api/orders/:id/deliver
+// @access  : Private/Admin
+
+const updateOrderToDelivered = asyncHandler(async(req, res) =>{
+    const order = await Order.findById(req.params.id)
+
+    if(order){
+        order.isDelivered = true
+        order.deliveredAt = Date.now()
+        
+        const updatedOrder = await order.save()
+        res.json( updatedOrder)
+    }else{
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})
 export { 
     addOrderItems,
     getOrderByID,
     updateOrderToPaid ,
     getMyOrders,
-    getAllOrders
+    getAllOrders,
+    updateOrderToDelivered
 }
