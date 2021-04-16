07f065e48d36da2ed8deddc0e90c1379760652fc
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-16 14:31:12 +0200

    fix: description effect correction

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col, Image, ListGroup, Card, Button, Form ,Collapse} from 'react-bootstrap'
+import { Row, 
+    Col, 
+    Image, 
+    ListGroup, 
+    Card, 
+    Button, 
+    Form ,
+    Collapse
+} from 'react-bootstrap'
 import Rating from '../components/Rating'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
