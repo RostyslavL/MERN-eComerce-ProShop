de731943b4c57247afe06f83b43c8a91c226cf07
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-16 13:05:51 +0200

    feat: create 'productReviewCreateReducer' reducer in productReducers

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -20,6 +20,11 @@ import {
   PRODUCT_UPDATE_SUCCESS,
   PRODUCT_UPDATE_FAIL,  
   PRODUCT_UPDATE_RESET,  
+
+  PRODUCT_CREATE_REVIEW_REQUEST,
+  PRODUCT_CREATE_REVIEW_SUCCESS,
+  PRODUCT_CREATE_REVIEW_FAIL,  
+  PRODUCT_CREATE_REVIEW_RESET, 
 } from '../constants/productConstants.js'
 
 export const productListReducer = (state = {products : [] }, action) =>{
@@ -85,16 +90,16 @@ export const productCreateReducer = (
     }
   }
   
-export const productUpdateReducer = (state = { product: {} }, action) => {
+export const productReviewCreateReducer = (state = { }, action) => {
   switch (action.type) {
-    case PRODUCT_UPDATE_REQUEST:
+    case PRODUCT_CREATE_REVIEW_REQUEST:
       return { loading: true }
-    case PRODUCT_UPDATE_SUCCESS:
-      return { loading: false, success: true, product: action.payload }
-    case PRODUCT_UPDATE_FAIL:
+    case PRODUCT_CREATE_REVIEW_SUCCESS:
+      return { loading: false, success: true  }
+    case PRODUCT_CREATE_REVIEW_FAIL:
       return { loading: false, error: action.payload }
-    case PRODUCT_UPDATE_RESET:
-      return { product: {} }
+    case PRODUCT_CREATE_REVIEW_RESET:
+      return { }
     default:
       return state
   }
