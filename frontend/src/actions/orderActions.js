7b250565190a5967097106026896761cb27af480
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-16 11:35:38 +0200

    fix: styles correction

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -203,7 +203,7 @@ export const deliverOrder = (order) => async (dispatch, getState) =>{
             },
         }
 
-        const { data } = await axios.put(`/api/orders/${order._id}/deliver`, config)
+        const { data } = await axios.put(`/api/orders/${order._id}/deliver`,{}, config)
 
         dispatch({
             type: ORDER_DELIVER_SUCCESS,
