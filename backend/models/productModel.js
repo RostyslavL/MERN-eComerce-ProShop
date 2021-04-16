1a4cdfbd114fe019e0ee7a3a971eb92f4aa7af4b
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-16 12:20:16 +0200

    fix: add a 'user' to reviewsSchema in productModel to associate user with a review

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -13,7 +13,12 @@ const reviewsSchema = mongoose.Schema({
     comment: {
         type : String,
         required: true,
-    }
+    },
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
 }, {
     timestamps: true
 })
