6457006c598d98581aa402acb1f3a950c73d08a4
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-16 16:13:00 +0200

    fix: component correction

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -1,35 +1,36 @@
-import React, {useState} from 'react'
-import {Form, Button} from 'react-bootstrap'
-
-const SearchBox = ({history}) => {
-
-    const [keyword, setKeyword] = useState('')
-    const submitHandler = (e) =>{
-        e.preventDefault()
-        if(keyword.trim()){
-            history.push(`/search/${keyword}`)
-        }else{
-            history.push('/')
-        }
+import React, { useState } from 'react'
+import { withRouter } from 'react-router-dom' 
+import { Form, Button } from 'react-bootstrap'
+ 
+const SearchBox = ({ history }) => {
+  const [keyword, setKeyword] = useState('')
+ 
+  const submithandler = (e) => {
+    e.preventDefault()
+    if (keyword.trim()) {
+      history.push(`/search/${keyword}`)
+    } else {
+      history.push(`/`)
     }
-
-    return (
-        <Form onSubmit={submitHandler} inline>
-            <Form.Control
-                className='mr-sm-2 ml-sm-5' 
-                type='text' 
-                name='q' 
-                onChange = {(e) => setKeyword(e.target.value)} 
-                placeholder='Search products...'>
-                    <Button 
-                        type='submin' 
-                        variant='outline-light' 
-                        className='p-2'> 
-                            Search 
-                        </Button>
-            </Form.Control>
-        </Form>
-    )
+  }
+ 
+  return (
+    <Form onSubmit={submithandler} inline>
+      <Form.Control
+        type='text'
+        name='q'
+        onChange={(e) => setKeyword(e.target.value)}
+        placeholder='Search Products...'
+        className='mr-sm-2 ml-sm-5'
+      ></Form.Control>
+      <Button 
+        type='submit' 
+        variant='outline-light' 
+        className='p-2'>
+        Search
+      </Button>
+    </Form>
+  )
 }
-
-export default SearchBox
+ 
+export default withRouter(SearchBox) 
