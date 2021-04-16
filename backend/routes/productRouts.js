53832ffc2f96a1afe02a39926ea19ddb40c3cd24
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-16 12:54:40 +0200

    feat: add 'productReview' protected route

--- backend/routes/productRouts.js
+++ backend/routes/productRouts.js
@@ -5,7 +5,8 @@ import {
     getProductById, 
     deleteProduct, 
     createProduct,
-    updateProduct
+    updateProduct,
+    createProductReview
 } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
@@ -13,9 +14,12 @@ router.route('/')
     .get(getProducts)
     .post(protect, admin, createProduct)
 
+router.route('/:id/reviews').post(protect, createProductReview)
+
 router.route('/:id')
     .get(getProductById)
     .put(protect, admin, updateProduct)
     .delete(protect, admin, deleteProduct)
+   
 
 export default router
