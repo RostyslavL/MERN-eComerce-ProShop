13795a99839aa6b0ce3d2e8f1c0c46429c882354
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-16 10:58:27 +0200

    feat:  create 'orderDeliverReducer'  in  orderReducers

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -1,25 +1,30 @@
 import {
-    ORDER_CREATE_REQUEST,
-    ORDER_CREATE_SUCCESS,
-    ORDER_CREATE_FAIL,
+  ORDER_CREATE_REQUEST,
+  ORDER_CREATE_SUCCESS,
+  ORDER_CREATE_FAIL,
 
-    ORDER_DETAILS_REQUEST,
-    ORDER_DETAILS_SUCCESS,
-    ORDER_DETAILS_FAIL,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
+  ORDER_DETAILS_FAIL,
 
-    ORDER_PAY_REQUEST,
-    ORDER_PAY_SUCCESS,
-    ORDER_PAY_FAIL,
-    ORDER_PAY_RESET,
+  ORDER_PAY_REQUEST,
+  ORDER_PAY_SUCCESS,
+  ORDER_PAY_FAIL,
+  ORDER_PAY_RESET,
 
-    ORDER_LIST_MY_FAIL, 
-    ORDER_LIST_MY_REQUEST, 
-    ORDER_LIST_MY_SUCCESS,
-    ORDER_LIST_MY_RESET,
+  ORDER_LIST_MY_FAIL, 
+  ORDER_LIST_MY_REQUEST, 
+  ORDER_LIST_MY_SUCCESS,
+  ORDER_LIST_MY_RESET,
 
-    ORDER_LIST_REQUEST,
-    ORDER_LIST_SUCCESS,
-    ORDER_LIST_FAIL,  
+  ORDER_LIST_REQUEST,
+  ORDER_LIST_SUCCESS,
+  ORDER_LIST_FAIL,
+
+  ORDER_DELIVER_REQUEST,
+  ORDER_DELIVER_SUCCESS,
+  ORDER_DELIVER_FAIL,
+  ORDER_DELIVER_RESET     
 
 } from '../constants/orderConstants.js'
 export const orderCreateReducers = (state = {}, action) =>{
@@ -142,6 +147,30 @@ export const orderListReducer = (
         return { 
           loading: false, error: action.payload 
         }
+      default:
+        return state
+    }
+  }
+
+export const orderDeliverReducer = (
+    state = {}, action) => {
+    switch (action.type) {
+      case ORDER_DELIVER_REQUEST:
+        return { 
+          loading: true
+        }
+      case ORDER_DELIVER_SUCCESS:
+        return { 
+          loading: false, 
+          orders: true
+        }
+      case ORDER_DELIVER_FAIL:
+        return { 
+          loading: false, 
+          error: action.payload 
+        }
+      case ORDER_DELIVER_RESET:
+        return {}
       default:
         return state
     }
