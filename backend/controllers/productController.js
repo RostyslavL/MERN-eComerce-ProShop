4d5aae1c372c597fe1fdf02a466e19ec45e8e4a2
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-16 12:56:22 +0200

    fixt:  'createProductReview' correction

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -101,35 +101,39 @@ const updateProduct = asyncHandler(async (req, res) => {
 
 // @desc    Create new review
 // @route   POST /api/products/:id/reviews
-// @access  Private 
+// @access  Private
 const createProductReview = asyncHandler(async (req, res) => {
-  const {
-   rating, commnent
-  } = req.body
+  const { rating, comment } = req.body
 
   const product = await Product.findById(req.params.id)
 
   if (product) {
-    const alreadyReviewed = product.reviews.find(review => review.user.toString() === req.user._id.toString())
+    const alreadyReviewed = product.reviews.find(
+      (r) => r.user.toString() === req.user._id.toString()
+    )
 
-    if(alreadyReviewed){
-        res.send(400)
-        throw new Error('Product already reviewed')
+    if (alreadyReviewed) {
+      res.status(400)
+      throw new Error('Product already reviewed')
     }
+
     const review = {
-        name: req.user.name,
-        rating: Number(rating),
-        commnent,
-        user: req.user._id
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
     }
-    products.reviews.push(review)
 
-    product.numReviews = products.reviews.length()
+    product.reviews.push(review)
 
-    product.rating = products.reviews.reduce((acc, item) => item.rating + acc, 0)/products.reviews.length
+    product.numReviews = product.reviews.length
+
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length
 
     await product.save()
-    res.status(201).json({message: 'Review added'}) // 201 - code  a new resource created
+    res.status(201).json({ message: 'Review added' }) // 201 - code : a new stuff added
   } else {
     res.status(404)
     throw new Error('Product not found')
