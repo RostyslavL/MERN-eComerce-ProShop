39c1a663bb284af984ee45e5bb89ce782b31f301
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-17 11:43:10 +0200

    fix: add 'USER_LOGIN_SUCCESS' & 'userInfo' into alocal storage after updating user profile

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -158,8 +158,14 @@ export const updateUserProfile = (user) => async (dispatch, getState) =>{
         )
         dispatch({
             type: USER_UPDATE_PROFILE_SUCCESS,
-            payload: data
+            payload: data,
         })
+        
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data,
+        })
+        localStorage.setItem('userInfo', JSON.stringify(data))
         
     } catch (error) {
         dispatch({
