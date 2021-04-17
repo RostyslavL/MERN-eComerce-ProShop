2a9d16baa40d5febfd9d15fb290cb5b5c30306f6
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-17 11:52:28 +0200

    feat: implementation user data update into ProfileScreen  use navbar

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -12,6 +12,7 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { getUserDetails, updateUserProfile } from '../actions/userActions'
 import { listMyOrders  } from '../actions/orderActions'
+import {USER_UPDATE_PROFILE_RESET} from '../constants/userConstants'
 
 const ProfileScreen = ({ location, history }) => {
     const [name, setName] = useState('')
@@ -38,7 +39,8 @@ const ProfileScreen = ({ location, history }) => {
         if(!userInfo){
             history.push('/login')
         }else{
-            if(!user.name){
+            if(!user.name || success){
+                dispatch({type: USER_UPDATE_PROFILE_RESET})
                 dispatch(getUserDetails('profile'))
                 dispatch(listMyOrders())
             }else{
@@ -46,7 +48,7 @@ const ProfileScreen = ({ location, history }) => {
                 setEmail(user.email)
             }
         }
-    }, [dispatch, history, userInfo, user])
+    }, [dispatch, history, userInfo, user, success])
     
     const submitHandler = (e) =>{
         e.preventDefault()
