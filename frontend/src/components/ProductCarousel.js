7318af1de0dcbde87f2ec984e2583ee2074c21dc
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-04-17 11:06:33 +0200

    fix: carousel correction

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -21,7 +21,11 @@ const ProductCarousel = () => {
   ) : error ? (
     <Message variant='danger'>{error}</Message>
   ) : (
-    <Carousel pause='hover' className='bg-dark'>
+    <Carousel 
+      pause='hover' 
+      className='bg-dark'
+      interval={1000}
+      >
       {products.map((product) => (
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
